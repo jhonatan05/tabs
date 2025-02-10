631b66f4e5f36ebe90a3f6c0263dca9416a0ed45
Jhonatan Arevalo <jhon20339@gmail.com>
2025-02-10 09:04:08 -0500

    Proyecto inicial

---
+++ custom.d.ts
@@ -0,0 +1,28 @@
+declare module "*.svg" {
+    const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+    export default content;
+  }
+  declare module '*.jpg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.png' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.jpeg' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.gif' {
+    const value: string;
+    export default value;
+  }
+  
+  declare module '*.svg' {
+    const content: string;
+    export default content;
+  }
